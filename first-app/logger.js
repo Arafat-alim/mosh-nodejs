6199e5010429f891c29cc578f39a8c7fd4488a32
arafat-alim <arafat97alim@gmail.com>
2022-07-30 01:32:55 +0530

    node wrapper function.

--- first-app/logger.js
+++ first-app/logger.js
@@ -1,8 +1,15 @@
+//! All codes are wrapped into a IIFE, this is a way a node module executed first
+
+// (function (exports, module, require, __dirname, __filename) {
 var url = "http://www.arafat-alim/log";
 
 function log(message) {
-  console.log(message);
+  console.log(message); //Arafat
 }
+
+console.log(__dirname); //E:\ARAFAT\Learning\Grinding\NodeJS\mosh-nodejs\first-app
+console.log(__filename); // E:\ARAFAT\Learning\Grinding\NodeJS\mosh-nodejs\first-app\logger.js
 // export import facilities
 module.exports = log;
 // module.exports.endPoints = url;
+// });
