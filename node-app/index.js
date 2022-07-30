a08df6d8af04d2a2957907bc8db4af3161e46555
arafat-alim <arafat97alim@gmail.com>
2022-07-31 00:02:25 +0530

    updated my existing package.

--- node-app/index.js
+++ node-app/index.js
@@ -2,3 +2,8 @@ const calc = require("cocoder-lib");
 
 const result = calc.add(12, 54);
 console.log("My addition Result - ", result);
+
+//updating my existing function
+//! after updating my cocoder-lib package
+const mul = calc.multiply(7, 5);
+console.log(mul);
