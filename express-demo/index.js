b7692fd35e1c326d89cd2f61c4ed233d3f9fc6a9
arafat-alim <arafat97alim@gmail.com>
2022-07-31 01:09:16 +0530

    environment variable concept added.

--- express-demo/index.js
+++ express-demo/index.js
@@ -13,5 +13,8 @@ app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
 
+//PORT
+const port = process.env.PORT || 3000;
 //PORT listen
-app.listen(3000, () => console.log("PORT is listening at 3000"));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
