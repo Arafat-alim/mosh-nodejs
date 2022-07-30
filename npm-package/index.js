fffbc9e369d4380134393b8fb74d8d13c54c56f9
arafat-alim <arafat97alim@gmail.com>
2022-07-30 19:38:27 +0530

    using a node module named underscore

---
+++ npm-package/index.js
@@ -0,0 +1,7 @@
+const _ = require("underscore");
+//! First find in CORE module
+//! Second find in Files and Folder
+//! Third find in nodeP_modules
+
+const result = _.contains([1, 2, 3], 3);
+console.log(result);
