5ff3ae609d53ef5c9b4734d723a41c4d9d5bf0b8
arafat-alim <arafat97alim@gmail.com>
2022-07-31 01:28:37 +0530

    params and query strings concept added.

--- express-demo/index.js
+++ express-demo/index.js
@@ -12,6 +12,23 @@ app.get("/", (req, res) => {
 app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
+//! Get all items
+app.get("/api/courses/", (req, res) => {
+  res.send();
+});
+//! Params
+app.get("/api/courses/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+app.get("/api/courses/:year/:month", (req, res) => {
+  res.send(req.params);
+});
+
+//! Query strings
+app.get("/api/posts/:year/:month", (req, res) => {
+  res.send(req.query);
+});
 
 //PORT
 const port = process.env.PORT || 3000;
