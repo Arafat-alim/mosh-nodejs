fd35d70aa30e8955bcbc2117066b12f0b707d70c
arafat-alim <arafat97alim@gmail.com>
2022-08-01 21:04:25 +0530

    vidly get request is successfully added

--- vidly/index.js
+++ vidly/index.js
@@ -3,10 +3,16 @@ const app = express();
 
 //Adding Middleeare
 app.use(express.json());
+//Adding dummy database
+const genres = [
+  { id: 1, name: "Horror" },
+  { id: 1, name: "Comedy" },
+  { id: 1, name: "action" },
+];
 
 //Create a home ENd point
 app.get("/api/genres", (req, res) => {
-  res.send("Hello World");
+  res.send(genres);
 });
 
 const port = process.env.PORT || 3000;
