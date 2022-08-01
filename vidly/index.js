99e07d52af6d1691a1a456394728059adbaaecd6
arafat-alim <arafat97alim@gmail.com>
2022-08-01 21:21:37 +0530

    post request without validation

--- vidly/index.js
+++ vidly/index.js
@@ -6,25 +6,28 @@ app.use(express.json());
 //Adding dummy database
 const genres = [
   { id: 1, name: "Horror" },
-  { id: 1, name: "Comedy" },
-  { id: 1, name: "action" },
+  { id: 2, name: "Comedy" },
+  { id: 3, name: "action" },
 ];
 
 //get all genres
-app.get("/api/genres", (req, res) => {
+app.get("/api/genres/", (req, res) => {
   res.send(genres);
 });
 
 //find particular genres
 app.get("/api/genres/:id", (req, res) => {
-  //look into the database
   const genre = genres.find((g) => g.id === parseInt(req.params.id));
   if (!genres) res.status(404).send("No Genre Found");
   res.send(genre);
 });
 
 //create a new genres
-app.post("/api/genres", (req, res) => {});
+app.post("/api/genres/", (req, res) => {
+  const genre = { id: genres.length + 1, name: req.body.name };
+  genres.push(genre);
+  res.send(genres);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, console.log(`Listening At ${port}`));
