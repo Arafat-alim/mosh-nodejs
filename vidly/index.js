6d54a23faf12a381023a9de8f76b2f65c595eccd
arafat-alim <arafat97alim@gmail.com>
2022-08-01 21:45:47 +0530

    updating Request (PUT) is working

--- vidly/index.js
+++ vidly/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 //Adding JOI
 const Joi = require("joi");
+const { validate } = require("joi/lib/types/object");
 
 //Adding Middleeare
 app.use(express.json());
@@ -37,6 +38,25 @@ app.post("/api/genres/", (req, res) => {
   const genre = { id: genres.length + 1, name: req.body.name };
 
   genres.push(genre);
+  res.send(genres);
+});
+
+//! Updating Request PUT
+app.put("/api/genres/:id", (req, res) => {
+  // look up the id
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre) {
+    return res.status(404).send("The Particular Genre Not Found");
+  }
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+  // inout validate
+  const result = Joi.validate(req.body, schema);
+  if (result.error)
+    return res.status(400).send(result.error.details[0].message);
+  // update
+  genre.name = req.body.name;
   res.send(genres);
 });
 
