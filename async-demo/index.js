a0a556ee6d478e2e5fb176bc6510d2f4ae3c21ab
arafat-alim <arafat97alim@gmail.com>
2022-08-03 22:10:53 +0530

    fix the problem!

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,7 +1,28 @@
-console.log("Before");
+//! The problemn we have to deal
 
-setTimeout(() => {
-  console.log("Reading data from the database");
-}, 2000);
+console.log("Before");
+const user = getUser(1);
+console.log(user); //undefined
+console.log("After");
+
+function getUser(id) {
+  setTimeout(() => {
+    console.log("Reading data from the database");
+    return { id: id, gitUserName: "Arafat" };
+  }, 2000);
+}
+
+//! Fixing the above code using callback function
+console.log("Before");
+getUser(1, function (user) {
+  console.log("User", user);
+});
 
 console.log("After");
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Reading data from the database");
+    callback({ id: id, name: "Mosh" });
+  }, 2000);
+}
