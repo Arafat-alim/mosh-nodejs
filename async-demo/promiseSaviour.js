effb71463fc22871c52ba110ac6d42334f5f719b
arafat-alim <arafat97alim@gmail.com>
2022-08-03 23:41:47 +0530

    code more flexible.

--- async-demo/promiseSaviour.js
+++ async-demo/promiseSaviour.js
@@ -1,12 +1,19 @@
 console.log("Before");
-getUser(1, (user) => {
-  getRepositories(user.gitHubUsername, (repos) => {
-    getCommits(repos[0], (commits) => {
-      console.log(commits);
-    });
-  });
-});
+// getUser(1, (user) => {
+//   getRepositories(user.gitHubUsername, (repos) => {
+//     getCommits(repos[0], (commits) => {
+//       console.log(commits);
+//     });
+//   });
+// });
 console.log("After");
+
+//! consuming Promise
+getUser(1)
+  .then((user) => getRepositories(user.gitHubUsername))
+  .then((repos) => getCommits(repos[0]))
+  .then((commit) => console.log("Commits -", commit))
+  .catch((err) => console.log("Error - ", err.message));
 
 function getUser(id) {
   return new Promise((resolve, rejected) => {
@@ -32,7 +39,7 @@ function getCommits(repo, callback) {
     setTimeout(() => {
       console.log("Calling GitHub API...");
       console.log(repo);
-      callback(["commit"]);
+      resolve(["commit"]);
     }, 2000);
   });
 }
