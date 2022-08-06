598e57111c5056ed3b22f9c8428bd3853ccaad51
arafat-alim <arafat97alim@gmail.com>
2022-08-07 01:28:25 +0530

    Program created by Arafat.

--- async-demo/exercise-two.js
+++ async-demo/exercise-two.js
@@ -11,36 +11,53 @@ getCustomer(1, (user) => {
 });
 */
 
-getCustomer(1)
-  .then((user) => isGoldMember(user.isGold))
-  .then((movie) => sentMail(movie))
-  .catch((err) => console.log("Error", err.message));
+// getCustomer(1)
+//   .then((user) => isGoldMember(user.isGold))
+//   .then((movie) => sentMail(movie))
+//   .then((msg) => console.log(msg))
+//   .catch((err) => console.log("Error", err.message));
 
+async function notifyCustomer() {
+  const user = await getCustomer(1);
+  console.log(user.name);
+
+  const isPrime = await isGoldMember(user.isGold);
+  console.log(movies);
+
+  const sentMail = await sentMail(movies);
+  console.log(msg);
+}
+
+notifyCustomer();
 function getCustomer(id) {
   return new Promise((resolve, rejected) => {
     setTimeout(() => {
       console.log("Get Customer is calling");
-      resolve({ id: id, name: "Arafat", isGold: false });
+      resolve({ id: id, name: "Arafat", isGold: true });
     }, 2000);
   });
 }
 function isGoldMember(user, callback) {
-  console.log("IsPrimeMembership: " + user);
-  setTimeout(() => {
-    if (user === true) {
-      callback(["Movie1", "Movie2", "Movie3"]);
-    } else {
-      callback(
-        new Error(
-          "Sorry You are not a Prime Member, please purchase Prime Member"
-        )
-      );
-    }
-  }, 2000);
+  return new Promise((resolve, rejected) => {
+    console.log("IsPrimeMembership: " + user);
+    setTimeout(() => {
+      if (user === true) {
+        resolve(["Movie1", "Movie2", "Movie3"]);
+      } else {
+        rejected(
+          new Error(
+            "Sorry You are not a Prime Member, please purchase Prime Member"
+          )
+        );
+      }
+    }, 2000);
+  });
 }
 
 function sentMail(movies, callback) {
-  setTimeout(() => {
-    callback(movies);
-  }, 2000);
+  return new Promise((resolve, rejected) => {
+    setTimeout(() => {
+      resolve(movies);
+    }, 2000);
+  });
 }
