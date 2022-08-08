71cb21e26765923938af77a3116a2262b3fac2b5
arafat-alim <arafat97alim@gmail.com>
2022-08-08 21:29:24 +0530

    schema created.

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -4,3 +4,11 @@ mongoose
   .connect("mongodb://127.0.0.1:27017/playground")
   .then(() => console.log("Connected to Mongodb"))
   .catch((err) => console.error("Database could not connected", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
