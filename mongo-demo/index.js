eb821598a25aa953383b1c59326856d15c3e801c
arafat-alim <arafat97alim@gmail.com>
2022-08-08 21:38:05 +0530

    created a model

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -5,10 +5,21 @@ mongoose
   .then(() => console.log("Connected to Mongodb"))
   .catch((err) => console.error("Database could not connected", err));
 
+//creating a schema
 const courseSchema = new mongoose.Schema({
   name: String,
   author: String,
   tags: [String],
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
+});
+
+//creting a model
+const Course = mongoose.model("Course", courseSchema);
+//creating an object
+const course = new Course({
+  name: "Node.js Course",
+  author: "Arafat",
+  tags: ["node", "backend"],
+  isPublished: true,
 });
