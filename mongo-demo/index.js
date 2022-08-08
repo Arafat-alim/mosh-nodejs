bdbd6fc46b687599354b59ea67a02548c8cd211c
arafat-alim <arafat97alim@gmail.com>
2022-08-08 22:12:10 +0530

    Querying documents

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -13,10 +13,10 @@ const courseSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
 });
+//creting a model
+const Course = mongoose.model("Course", courseSchema);
 
 async function createCourse() {
-  //creting a model
-  const Course = mongoose.model("Course", courseSchema);
   //creating an object
   const course = new Course({
     name: "Angular Course",
@@ -30,4 +30,14 @@ async function createCourse() {
   console.log(result); // it shows us a unique Indentifier
 }
 
-createCourse();
+//Querying Documents
+async function getCourses() {
+  const courses = await Course.find()
+    .limit(10)
+    .sort({ name: 1 }) // +1 indicated ascending order, -1 inditcate descending order
+    .select({ name: 1, tags: 1 }); //showing the properties
+  console.log(courses);
+}
+
+// createCourse();
+getCourses();
