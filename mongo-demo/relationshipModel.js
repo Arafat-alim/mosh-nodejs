e3203b6e124e1c9f527488007d3714d5d69eb764
arafat-alim <arafat97alim@gmail.com>
2022-08-11 14:10:11 +0530

    relationship models

---
+++ mongo-demo/relationshipModel.js
@@ -0,0 +1,28 @@
+//! using Reference (Normalization) --> CONSISTENCY
+let author = {
+  name: "Arafat",
+};
+
+let course = {
+  author: "id",
+};
+
+//! using embedded document (DeNormalization) --> PERFORMANCE
+
+let course = {
+  author: {
+    name: "Arafat",
+  },
+};
+
+//! !Hybrid
+let author = {
+  name: "Arafat",
+};
+
+let course = {
+  author: {
+    id: "ref",
+    author: "arafat",
+  },
+};
